22b8de2cb667f9ae0a10b37ddbdc0c56b3facd83
Kerolous Botrus <bkerolous@gmail.com>
2025-06-25 13:57:59 +0300

    delete read me folder

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import "./App.css";
 import Home from "./Components/Home/Home";
 import Favorites from "./Components/Favorite/Favorite";
 import Layout from "./Components/Layout/Layout";
-import ReadMe from "./Components/ReadMe/ReadMe";
 import Details from "./Components/Details/Details";
 
 function App() {
@@ -14,7 +13,6 @@ function App() {
         { path: "", element: <Home /> },
         { path: "favorites", element: <Favorites /> },
         { path: "details/:id/:media_type", element: <Details /> },
-        { path: "readme", element: <ReadMe /> },
       ],
     },
   ]);
