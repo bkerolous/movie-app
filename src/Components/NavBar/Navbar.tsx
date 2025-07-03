072c441e54848cc275a5bd545aed3dd232f8e66a
Kerolous Botrus <bkerolous@gmail.com>
2025-07-03 12:59:50 +0300

    Refactor Navbar component logic and structure for better mobile navigation

--- src/Components/NavBar/Navbar.tsx
+++ src/Components/NavBar/Navbar.tsx
@@ -1,44 +1,81 @@
 import { Link } from "react-router-dom";
+import { FaBars } from "react-icons/fa";
 import style from "./Navbar.module.scss";
+import { useState } from "react";
 
 interface NavBarProps {
   setSearch?: React.Dispatch<React.SetStateAction<string>>;
 }
 
 const NavBar = ({ setSearch }: NavBarProps) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const menueState = () => setIsOpen((prev) => !prev);
+
   return (
-    <>
-      <nav className={`${style.container}`}>
-        <div className={`${style.logo}`}>
-          <Link className={`${style.link}`} to="./">
-            Tranding Movies
-          </Link>
+    <nav className={style.container}>
+      <div className={style.logo}>
+        <Link className={style.link} to="./">
+          Trending Movies
+        </Link>
+      </div>
+
+      {setSearch && (
+        <div className={style.search}>
+          <input
+            onChange={(e) => setSearch(e.target.value)}
+            type="search"
+            placeholder="Search By Name"
+          />
         </div>
-        {setSearch && (
-          <div className={`${style.search}`}>
-            <input
-              onChange={(e) => setSearch(e.target.value)}
-              type="search"
-              placeholder="Search By Name"
-            />
-          </div>
-        )}
-        <div>
-          <ul className={`${style["nav-right"]}`}>
+      )}
+
+      <ul className={style["nav-right"]}>
+        <li>
+          <Link className={style.link} to="./">
+            Home
+          </Link>
+        </li>
+        <li>
+          <Link className={style.link} to="./favorites">
+            Favorite List
+          </Link>
+        </li>
+      </ul>
+
+      <FaBars className={style["menu-bar"]} onClick={menueState} />
+
+      {isOpen && (
+        <div className={style.mobileMenu}>
+          <ul className={style['nav-right-mobile']}>
             <li>
-              <Link className={`${style.link}`} to="./">
+              <Link className={style.link} to="./">
                 Home
               </Link>
             </li>
             <li>
-              <Link className={`${style.link}`} to="./favorites">
+              <Link className={style.link} to="./favorites">
                 Favorite List
               </Link>
             </li>
+          <li>
+            
+            {setSearch && (
+            <div className={style['mobileMenu-search']}>
+              <input
+                onChange={(e) => setSearch(e.target.value)}
+                type="search"
+                placeholder="Search By Name"
+              />
+            </div>
+          )}
+
+          </li>
           </ul>
         </div>
-      </nav>
-    </>
+      )}
+
+    </nav>
   );
 };
 
