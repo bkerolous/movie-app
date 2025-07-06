6381137011b939e4f792d87e99fd7ea28268f515
Kerolous Botrus <bkerolous@gmail.com>
2025-07-06 14:38:45 +0300

    Refactor Layout: remove local favorites state in favor of TMDB API

--- src/Components/Layout/Layout.tsx
+++ src/Components/Layout/Layout.tsx
@@ -2,26 +2,16 @@ import { Outlet, useLocation } from "react-router-dom";
 import Footer from "../Footer/Footer";
 import NavBar from "../NavBar/Navbar";
 import { useState } from "react";
-import type { movies } from "../../type/interface";
 
 const Layout = () => {
   const [search, setSearch] = useState("");
-  const [favorites, setFavorites] = useState<movies[]>([]);
-  const location = useLocation()
-  const isHome = location.pathname === '/'
-
-  const toggleFavorite = (movie: movies) => {
-    setFavorites((prev) =>
-      prev.some((fav) => fav.id === movie.id)
-        ? prev.filter((fav) => fav.id !== movie.id)
-        : [...prev, movie]
-    );
-  };
+  const location = useLocation();
+  const isHome = location.pathname === "/";
 
   return (
     <>
-      <NavBar {...(isHome ?{setSearch} : {})} />
-      <Outlet context={{ favorites, toggleFavorite, search }} />
+      <NavBar {...(isHome ? { setSearch } : {})} />
+      <Outlet context={{ search }} />
       <Footer />
     </>
   );
