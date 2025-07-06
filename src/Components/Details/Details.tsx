8424a5d54bc852680acd843d890c25667667a28f
Kerolous Botrus <bkerolous@gmail.com>
2025-07-06 14:37:49 +0300

    Update movie details display structure in Details component

--- src/Components/Details/Details.tsx
+++ src/Components/Details/Details.tsx
@@ -6,16 +6,23 @@ import { ImSpinner6 } from "react-icons/im";
 const Details = () => {
   const { id, media_type } = useParams();
   const movieId = Number(id);
-  const movieType = media_type as string
-  const { data, isError, isLoading, error } = useDetails(movieId,movieType);
-  if (isLoading) return <div className={`${style['spinner-wrap']}`}><ImSpinner6 className={`${style.spinner}`} /></div>;
+  const movieType = media_type as string;
+  const { data, isError, isLoading, error } = useDetails(movieId, movieType);
+  if (isLoading)
+    return (
+      <div className={`${style["spinner-wrap"]}`}>
+        <ImSpinner6 className={`${style.spinner}`} />
+      </div>
+    );
   if (isError) return <p>{(error as Error).message}</p>;
+  console.log(data);
+
   return (
     <>
       {
         <div className={`${style.container}`}>
           <div className={`${style["card"]}`}>
-            <div className={`${style['card-movie']}`}>
+            <div className={`${style["card-movie"]}`}>
               <div>
                 <img
                   className={`${style["card-img"]}`}
@@ -30,16 +37,24 @@ const Details = () => {
                   {data?.title}
                 </h3>
                 <p>
+                  <span className={`${style["text-size"]}`}>
+                    Permissible Age:{" "}
+                  </span>
+                  {data?.adult ? "+ 16" : "+ 10"}
+                </p>
+                <p>
                   <span className={`${style["text-size"]}`}>Vote : </span>
                   {data?.vote_average.toFixed(1)}
                 </p>
                 <p className="card-media_type">
-                  <span className={`${style["text-size"]}`}>Media_type : </span>
-                  {media_type}
+                  <span className={`${style["text-size"]}`}>Genres : </span>
+                  {data?.genres[0].name}, {data?.genres[1].name},{" "}
+                  {data?.genres[2].name}
                 </p>
                 <p className="card-overview">
-                    <span className={`${style["text-size"]}`}>Over_View : </span>
-                     {data?.overview}</p>
+                  <span className={`${style["text-size"]}`}>Over View : </span>
+                  {data?.overview}
+                </p>
               </div>
             </div>
           </div>
