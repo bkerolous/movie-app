fecc6a173ade67765cafbdc4c3afef321b90393f
Kerolous Botrus <bkerolous@gmail.com>
2025-07-06 15:19:22 +0300

    Refactor: simplify favorites component and remove duplicate

--- src/Components/Favorite/Favorite.tsx
+++ src/Components/Favorite/Favorite.tsx
@@ -1,56 +1,66 @@
-import { useOutletContext } from "react-router-dom";
-import type { movies } from "../../type/interface";
 import { MdFavorite } from "react-icons/md";
 import style from "../../Styles/App.module.scss";
-interface OutletContext {
-  favorites: movies[];
-  toggleFavorite: (movie: movies) => void;
-}
+import { useMutationFav } from "../../api-hooks/usePost-favorite";
+import { useGetFav } from "../../api-hooks/useGetFav";
+import type { movies } from "../../type/interface";
+import { ImSpinner6 } from "react-icons/im";
+
 const Favorites = () => {
-  const { favorites, toggleFavorite } = useOutletContext<OutletContext>();
-  const isMovieFav = (movie: movies) =>
-    favorites.some((fav) => fav.id === movie.id);
+  const { data: favorites = [], isLoading, isError, error } = useGetFav();
+  const { mutate } = useMutationFav();
+
+  const handleToggleFavorite = (movie: movies) => {
+    mutate({ movie, isFav: false });
+  };
+
+  if (isLoading) {
+    return (
+      <div className={`${style["spinner-wrap"]}`}>
+        <ImSpinner6 className={`${style.spinner}`} />
+      </div>
+    );
+  }
+
+  if (isError) return <p>{(error as Error).message}</p>;
 
   return (
-    <>
-      <div className={`${style.container}`}>
-        {favorites.length === 0 && (
-          <div className={`${style['no-favorite']}`}>
-            <p>No Favorite Added yet.</p>
-          </div>
-        )}
-        <div className={`${style["movie-card"]}`}>
-          {favorites &&
-            favorites.map((movie: movies) => (
-              <div className={`${style.card}`} key={movie.id}>
-                <img
-                  className={`${style["card-img"]}`}
-                  src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
-                  alt={movie.title}
-                />
-                <button
-                  onClick={() => toggleFavorite(movie)}
-                  type="button"
-                  className={`${style["card-favorit"]}`}
-                  title="Remove From Favorite"
-                >
-                  <MdFavorite
-                    className={isMovieFav(movie) ? style.active : ""}
-                  />
-                </button>
-                <span className={`${style["card-vote"]}`}>
-                  Vote : {movie.vote_average}
-                </span>
-                <h3 className="card-title">Title : {movie.title}</h3>
-                <p className="card-media_type">
-                  Media_type : {movie.media_type}
-                </p>
-                <p className="card-overview">Overview : {movie.overview}</p>
-              </div>
-            ))}
+    <div className={`${style.container}`}>
+      {favorites.length === 0 && (
+        <div className={`${style["no-favorite"]}`}>
+          <p>No Favorite Added yet.</p>
         </div>
+      )}
+      <div className={`${style["movie-card"]}`}>
+        {favorites.map((movie: movies) => (
+          <div className={`${style.card}`} key={movie.id}>
+            <img
+              className={`${style["card-img"]}`}
+              src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
+              alt={movie.title}
+            />
+            <button
+              onClick={() => handleToggleFavorite(movie)}
+              type="button"
+              className={`${style["card-favorit"]}`}
+              title="Remove From Favorite"
+            >
+              <MdFavorite className={`${style.active}`} />
+            </button>
+            <h3>Title : {movie.title}</h3>
+            <p>
+              <span>Permissible Age: </span>
+              {movie?.adult ? "+ 16" : "+ 10"}
+            </p>
+            <p>
+              <span>Genres : </span>
+              {movie?.genres?.map((genre) => `${genre.name}, `)}
+            </p>
+            <span>Vote : {movie.vote_average.toFixed(1)}</span>
+            <p>Overview : {movie.overview}</p>
+          </div>
+        ))}
       </div>
-    </>
+    </div>
   );
 };
 
