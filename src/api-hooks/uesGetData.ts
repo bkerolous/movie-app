f1bdc319bce9102770ecb73e734ea6b450e12fd8
Kerolous Botrus <bkerolous@gmail.com>
2025-07-09 10:40:37 +0300

    Update useGetData hook to support pagination using API page param

--- src/api-hooks/uesGetData.ts
+++ src/api-hooks/uesGetData.ts
@@ -2,14 +2,19 @@ import type { movies } from "../type/interface";
 import { useQuery } from "@tanstack/react-query";
 import api from "../utils/axoisInstance";
 
-const getData = async () => {
-  const { data } = await api.get(`/trending/movie/week`);
-  return data.results as movies[];
+const getData = async (page: number) => {
+  const { data } = await api.get(`/trending/movie/week`, {
+    params: {page},
+  });
+  return {
+    results: data.results as movies[],
+    total_pages: data.total_pages,
+  };
 };
 
-export const useGetData = () => {
+export const useGetData = (page: number) => {
   return useQuery({
-    queryKey: ["movies"],
-    queryFn: getData,
+    queryKey: ["movies", page],
+    queryFn: () => getData(page),
   });
 };
